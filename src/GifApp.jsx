3170349b4b8ec2a969bb60dec154341374a202cd
Jesus Correa <jesusippog@gmail.com>
2023-12-15 15:07:50 -0500

    creating gif grid and addcategory

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifApp = () => {
-    const [categories, setCategories] = useState([ 'Kanye', 'West'] );
+  const [categories, setCategories] = useState(["Kanye", "West"]);
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([...categories, newCategory]);
+  };
   return (
     <>
-    <h1>GifApp</h1>
+      <h1>GifApp</h1>
 
-    <ol>
-        { categories.map( category => {
-            return <li key={ category } > {category} </li>
-        }) }
-        {/* <li>
-            ABC
-        </li> */}
-    </ol>
+      <AddCategory onNewCategory={(value) => onAddCategory(value)} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
-  )
-}
+  );
+};
